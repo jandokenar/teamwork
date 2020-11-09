d4bf7a3d89633829d995c4f528b350e8cc7b952a
Matti P <mpuupera@gmail.com>
2020-11-09 14:02:41 +0200

    Adding book router

---
+++ server/src/routes/bookRouter.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+    newBook,
+    getBook,
+    getBooks,
+    deleteBook,
+    modifyBook,
+    searchBook,
+    reserverBook,
+} from "../controllers/bookController.js";
+
+const bookRouter = express.Router();
+
+bookRouter.get("/", (req, res) => {
+    res.send("hello from library");
+});
+
+bookRouter.post("/book", newBook);
+bookRouter.get("/book", getBook);
+bookRouter.get("books", getBooks);
+bookRouter.delete("book", deleteBook);
+bookRouter.put("book", modifyBook);
+bookRouter.put("book/search", searchBook);
+bookRouter.put("book/reserve", reserverBook);
+
+export default bookRouter;
