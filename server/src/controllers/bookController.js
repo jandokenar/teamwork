e9ad9d3540e6c405865f0d68586148161e97e94d
Henrik Peteri <P.Henrik.Rikhard@hotmail.com>
2020-11-10 11:07:04 +0200

    Fixing typo in bookContoller.js

--- server/src/controllers/bookController.js
+++ server/src/controllers/bookController.js
@@ -1,6 +1,6 @@
 import bookModel from "../models/bookModel.js";
 
-export async function GetBookOrFailt(req,res) {
+export async function GetBookOrFail(req,res) {
     const book = await bookModel.findOne(req.body.filter).exec();
     if (book) {
         res.status(200).json(book);
