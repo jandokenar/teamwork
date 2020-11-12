181476078bc43899fb0a67fc30f19eb015d06409
Arto Kujala <artokjl@gmail.com>
2020-11-12 14:34:52 +0200

    fixed getbook and deleted getBooks duplicate

--- server/src/routes/bookRouter.js
+++ server/src/routes/bookRouter.js
@@ -1,22 +1,21 @@
 import express from "express";
 import {
     addBook,
-    GetBookOrFail,
+    getBook,
     updateBook,
     getBooks,
     deleteBook,
 } from "../controllers/bookController.js";
+
 const bookRouter = express.Router();
 
 bookRouter.post("/", addBook);
-bookRouter.get("/", GetBookOrFail);
+bookRouter.get("/", getBook);
 bookRouter.put("/", updateBook);
 bookRouter.get("/all/", getBooks);
-/*
 bookRouter.delete("/", deleteBook);
-bookRouter.put("/", updateBook);
 
-bookRouter.get("/all", getBooks);
+/*
 bookRouter.get("/search", searchBook);
 bookRouter.post("/reserve", reserverBook);
 */
