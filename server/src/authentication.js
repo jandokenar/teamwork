ab41d9b0423fe666665fbe4d6ee25b393a7d6b1e
Henrik Peteri <P.Henrik.Rikhard@hotmail.com>
2020-11-25 12:39:25 +0200

    Adding authentication to backend. Also adding middleware for cors

---
+++ server/src/authentication.js
@@ -0,0 +1,65 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import UserModel from "./models/userModel.js";
+
+const secretKey = "mysecretkey";
+const refreshSecretKey = "mysecretkey";
+const AT_EXPIRE_TIME = "15min";
+const RT_EXPIRE_TIME = "1h";
+
+//@NOTE:
+//accesstoken has UserModel.id
+
+//When 'accessToken' is authenticated. User is added to request as user (request.account)
+//When 'refreshToken' is authenticated, UserID is added to request as decoded (request.decoded)
+export const CreateTokens = (userID) => (
+    {
+        token: jwt.sign({ userID }, secretKey, { expiresIn: AT_EXPIRE_TIME }),
+        refreshToken: jwt.sign({ userID }, refreshSecretKey, { expiresIn: RT_EXPIRE_TIME }),
+    }
+);
+export const RenewAccessToken = (req, res) => {
+    // Renew access token while refresh token is valid.
+    const tokens = CreateTokens(req.body.decoded.userID);
+    res.status(200).json({ token: tokens.token });
+};
+export const AuthenticateLocal = (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(403).json({ Error: "Not Authorized" });
+    }
+    UserModel.findOne({ id: req.body.userID }).then(async (it) => {
+        if (!it) return res.status(403).json({ Error: "Not Found" });
+        const isMatch = bcrypt.compareSync(req.body.password, it.password);
+        if (!isMatch) return res.status(403).json({ Error: "Not Authorized" });
+        
+        next();
+    });
+    return;
+};
+export const AuthenticateAccessToken = (req, res, next) => {
+    if (!req.headers.authentication) return res.status(403).json({ Error: "NoToken" });
+    const token = req.headers.authentication.split(" ")[1];
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(403).json(err);
+        }
+        UserModel.findOne({ id: decoded.userID }).then(async (it) => {
+            if (!it) return res.status(403).json({ Error: "User Not Found" });
+
+            req.body = { ...req.body,  user: it };
+            next();
+        });
+    });
+};
+export const AuthenticateRefreshToken = (req, res, next) => {
+    const {
+        cookies,
+    } = req;
+    jwt.verify(cookies.refreshToken, refreshSecretKey, (err, decoded) => {
+        if (err) {
+            return res.status(403).json(err);
+        }
+        req.body = { ...req.body, decoded };
+    });
+    next();
+};
