c7ba35afd3f6f2c109e6587119977095ff259065
Arto Kujala <artokjl@gmail.com>
2020-11-30 15:09:15 +0200

    Added book search to all books

--- frontend/src/components/bookView.jsx
+++ frontend/src/components/bookView.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from 'react';
 import { GetAllBooks } from "../APIWrapper.js";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../css/styleSheet.css";
 
 const BookView = () => {
     const [books, setBooks] = useState([]);
     const [booksUpdate, setBookUpdate] = useState(false);
+    const [search, setSearch] = useState("");
+
+    const showSearchedBooks = books.filter(book => book.author.toLowerCase().startsWith(search.toLowerCase()));
 
     const UpdateBookView = () => {
         if (booksUpdate) {
-            return (books.map((key, index) =>
-                <div key={index}>
-                    <Link key={index} to={"book?isbn=" + key.isbn}>
-                        {
-                            `${key.author}: ${key.title} (${key.published.substring(0, 4)})`
-                        }
-                    </Link>
+            return (
+                <div>
+                    Search book (author): <input value={search} onChange={(e) => setSearch(e.target.value)} />
+                {showSearchedBooks.map((key, index) =>
+                    <div key={index}>
+                        <Link key={index} to={"book?isbn=" + key.isbn}>
+                            {
+                                `Author: ${key.author} Title: ${key.title} (${key.published.substring(0, 4)})`
+                            }
+                        </Link>
+                    </div>
+                    )}
                 </div>
-            ));
+            );
         } else {
             setBookUpdate(true);
             GetAllBooks(setBooks);
