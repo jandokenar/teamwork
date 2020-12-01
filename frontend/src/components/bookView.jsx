c78b66956cca66b1c9ffe6f960a6d24254354c3d
Arto Kujala <artokjl@gmail.com>
2020-12-01 10:01:59 +0200

    bookSearch fixed

--- frontend/src/components/bookView.jsx
+++ frontend/src/components/bookView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GetAllBooks } from "../APIWrapper.js";
 import { Link } from "react-router-dom";
 
@@ -6,31 +6,49 @@ import "../css/styleSheet.css";
 
 const BookView = () => {
     const [books, setBooks] = useState([]);
-    const [booksUpdate, setBookUpdate] = useState(false);
     const [search, setSearch] = useState("");
+    const [searchBy, setSearchBy] = useState("author");
 
-    const showSearchedBooks = books.filter(book => book.author.toLowerCase().startsWith(search.toLowerCase()));
+    useEffect(() => {
+        GetAllBooks(setBooks);
+    }, [])
+
+    const sortedBooks = books.sort((a, b) => {
+        const authorA = a.author.toLowerCase();
+        const authorB = b.author.toLowerCase();
+
+        if (authorA < authorB) {
+            return -1;
+        }
+        if (authorA > authorB) {
+            return 1;
+        }
+        return 0;
+    });
+    
+    const showSearchedBooks = sortedBooks.filter(book => book[searchBy].toLowerCase().startsWith(search.toLowerCase()));
 
     const UpdateBookView = () => {
-        if (booksUpdate) {
-            return (
-                <div>
-                    Search book (author): <input value={search} onChange={(e) => setSearch(e.target.value)} />
-                {showSearchedBooks.map((key, index) =>
-                    <div key={index}>
-                        <Link key={index} to={"book?isbn=" + key.isbn}>
-                            {
-                                `${key.author}: ${key.title} (${key.published.substring(0, 4)})`
-                            }
-                        </Link>
-                    </div>
-                    )}
-                </div>
-            );
-        } else {
-            setBookUpdate(true);
-            GetAllBooks(setBooks);
-        }
+        return (
+            <div>
+                Search book by <select onChange={(e) => setSearchBy(e.target.value)}>
+                    <option value="author">Author</option>
+                    <option value="title">Title</option>
+                    <option value="isbn">ISBN</option>
+                    </select>: 
+                    <input placeholder="Enter value here" value={search} onChange={(e) => setSearch(e.target.value)} />
+                    {showSearchedBooks.length < 21 ?
+                    showSearchedBooks.map((key, index) =>
+                        <div key={index}>
+                            <Link key={index} to={"book?isbn=" + key.isbn}>
+                                {
+                                    `${key.author}: ${key.title} (${key.published.substring(0, 4)})`
+                                }
+                            </Link>
+                        </div>
+                    ) : <div></div>}
+            </div>
+        );
     }
 
 
