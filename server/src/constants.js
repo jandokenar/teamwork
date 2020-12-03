2a8267c8c424e4c8744fb4a986fa140fac2db81a
Matti P <mpuupera@gmail.com>
2020-12-03 10:02:06 +0200

    fixes for #53 constants.js

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,2 +1,2 @@
 export const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-export const weeks = 12096e5; // 2 week loan in ms
+export const borrowTime = 12096e5; // 2 week loan in ms
